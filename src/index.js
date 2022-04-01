0982cbed783defef685b888d3cb5c1fc05e2aeda
vinsifa <vinpratama12@gmail.com>
2022-04-01 12:00:03 +0700

    Pertemuan 6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose} from 'redux'
+import MainReducer from './reducers/MainReducer'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f) (createStore)(MainReducer)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = {store}>
+    <MainReducer />
+  </Provider>,
   document.getElementById('root')
 );
 
@@ -15,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
